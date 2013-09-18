36d0f1d52fb941e5585ecef9626fa2b87f2f0c70
Benjamin Dawson <bendawson.rb@gmail.com>
2013-09-18 16:22:32 +1000

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+gem 'haml-rails'
+gem 'thin'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'letter_opener'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+gem 'jquery-rails'
+gem 'carrierwave'
+gem 'carrierwave-video'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileUploaderConverter::Application.config.secret_token = '3b1800b538a845ff73006f03684f27dee2c6e8f8024a26f00c581f7b8afd435740d97ff810a6a5b826c22be2373d071fe22e2e9ddd582de4efef49bd61085db6'
